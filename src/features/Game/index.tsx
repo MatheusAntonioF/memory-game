5b3faf21db11d171c6e15cda63668f72daaec964
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2023-03-24 20:03:00 -0300

    feat: validation to match cards

--- src/features/Game/index.tsx
+++ src/features/Game/index.tsx
@@ -5,49 +5,81 @@ type ICard = number[][];
 type ICardPosition = {
   row: number;
   column: number;
+  cardNumber: number;
 };
 
-export function Game(): JSX.Element {
-  const [cards, setCards] = useState<ICard>([
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [5, 5, 6, 6],
-    [7, 7, 8, 8],
-  ]);
+const cards: ICard = [
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [5, 5, 6, 6],
+  [7, 7, 8, 8],
+];
 
+export function Game(): JSX.Element {
   const [revealedCards, setRevealedCards] = useState<ICardPosition[]>([]);
+  const [wasPlayerWin, setWasPlayerWin] = useState<boolean>(false);
+
+  const [matchedCardsNumber, setMatchedCardsNumber] = useState<number[]>([]);
+
+  function playerWon() {}
+
+  function matchCards(firstCard: ICardPosition, secondCard: ICardPosition) {
+    if (firstCard.cardNumber !== secondCard.cardNumber) return;
+
+    setMatchedCardsNumber(prevMatchedCards => {
+      return [...prevMatchedCards, firstCard.cardNumber];
+    });
+
+    playerWon();
+  }
 
   function onCardClick(cardPosition: ICardPosition) {
-    setRevealedCards(prevRevealed => {
-      if (prevRevealed.length === 2) return [cardPosition];
+    let cardsToBeRevealed = [];
 
-      return [...prevRevealed, cardPosition];
-    });
+    if (revealedCards.length === 2 || revealedCards.length === 0) {
+      cardsToBeRevealed = [cardPosition];
+    } else {
+      cardsToBeRevealed = [...revealedCards, cardPosition];
+
+      matchCards(cardsToBeRevealed[0], cardsToBeRevealed[1]);
+    }
+
+    setRevealedCards(cardsToBeRevealed);
+  }
+
+  function isRevealedCard({ row, column, cardNumber }: ICardPosition) {
+    if (matchedCardsNumber.includes(cardNumber)) return true;
+
+    let isRevealed = false;
+
+    if (revealedCards.length > 0) {
+      isRevealed = !!revealedCards.find(
+        card => card.column === column && card.row === row
+      );
+    }
+
+    return isRevealed;
   }
 
   return (
     <div className="grid gap-2 grid-cols-4 grid-rows-4 w-full h-full">
       {cards.map((row, rowIndex) => {
         return row.map((cardNumber, columnIndex) => {
-          console.log("🚀 ~ revealedCards:", revealedCards);
-
-          const isRevealedCard =
-            revealedCards.length > 0
-              ? revealedCards.find(
-                  card => card.column === columnIndex && card.row === rowIndex
-                )
-              : false;
-
           return (
             <button
               key={`card_${rowIndex}_${columnIndex}`}
               className="bg-slate-300 rounded grid place-items-center text-2xl hover:bg-slate-400"
               onClick={() =>
-                onCardClick({ row: rowIndex, column: columnIndex })
+                onCardClick({ row: rowIndex, column: columnIndex, cardNumber })
               }
             >
-              {/* {cardNumber} */}
-              {isRevealedCard ? cardNumber : ""}
+              {isRevealedCard({
+                row: rowIndex,
+                column: columnIndex,
+                cardNumber: cardNumber,
+              })
+                ? cardNumber
+                : ""}
             </button>
           );
         });
